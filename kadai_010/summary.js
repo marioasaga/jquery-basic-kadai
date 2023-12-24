f7cc03c0241947c8abdde241fcd795b7cbbd2021
marioasaga <149277298+marioasaga@users.noreply.github.com>
2023-12-25 06:02:47 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,28 @@
+$(function(){
+
+  // id属性がchange-colorの要素がクリックされたら
+  $('#change-color').on('click', function () {
+    // 文字色が変化
+      $('#target').css('color', 'red');
+  });
+
+  // id属性がchange-textの要素がクリックされたら
+  $('#change-text').on('click', function () {
+    // 文字内容が変化
+     $('#target').text('Hello!');
+  });
+
+  // id属性がfade-outの要素がクリックされたら
+  $('#fade-out').on('click', function () {
+    // フェードアウトで文字が消える
+     $('#target').fadeOut();
+  });
+
+  // id属性がfade-inの要素がクリックされたら
+  $('#fade-in').on('click', function () {
+    // フェードインで文字が現れる
+     $('#target').fadeIn();
+  });
+
+
+});
